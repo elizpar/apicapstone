39117faf47d19e7f3de13dd8d5bfba47233c3353
Liz <elizpar@gmail.com>
2015-09-16 19:36:27 -0400

    change base js code to youtubeapi template

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
+$
 $(function(){
 
   $("#search-term").submit(function(event){
     event.preventDefault();
     var searchTerm = $('#query').val();
-    getRequest(searchTerm);
-    
-    function getRequest(searchTerm) {
-      $.getJSON('http://www.omdbapi.com/?s=' + searchTerm + '&r=json', function(data){
-      var myData = data.Search;
-      showResults(myData);
-      console.log(myData);
-      });
-    }
 
-  	function showResults(results){
-	    for (var i = 0; i < results.length; i++) {
-         $('.search-results').append('<span>' + results[i].Title + '</span><br>');
-         console.log(results[i].Title);
-	       };
-    	}
-    
-  });
+    var params = {
+      part: 'snippet',
+      key: 'AIzaSyDDVUcMpTZeyPo0UTVB0wRlZVsc1Q00zxk',
+      q: searchTerm,
+      r: 'json'
+    };
 
+    $.getJSON('https://www.googleapis.com/youtube/v3/search', params, function(data){
+    var myData = data.items[0].snippet.thumbnails.default.url;
+    console.log(myData);
+    showResults(data.items);
+     
+    function showResults(results){
+      $('.search-results').empty();
+      for (var i = 0; i < results.length; i++) {
+        var vidId = results[i].id.videoId;
+        console.log(vidId);
+        var addThumbnail = '<span><a href="https://www.youtube.com/watch?v='+ vidId +'"><img src="https://i.ytimg.com/vi/' + vidId+ '/default.jpg"></a></span><p>';
+        $('.search-results').append(addThumbnail);
+        console.log(vidId);
+        };
+      }
+    });
+  });  
+});
 
-});  
+oauth: c9c6541be312c9977298caed1b3057308ac1c638
