75acbbc102e066ce1a59c8b8e4a9d2c052dde1b7
Liz <elizpar@gmail.com>
2015-09-18 10:15:37 -0400

    add gitignore

--- app.js
+++ app.js
@@ -6,29 +6,27 @@ $(function(){
     var searchTerm = $('#query').val();
 
     var params = {
-      part: 'snippet',
-      key: 'AIzaSyDDVUcMpTZeyPo0UTVB0wRlZVsc1Q00zxk',
+      part: '',
+      key: 'auth.js',
       q: searchTerm,
       r: 'json'
     };
 
-    $.getJSON('https://www.googleapis.com/youtube/v3/search', params, function(data){
-    var myData = data.items[0].snippet.thumbnails.default.url;
+    $.getJSON('https://api.github.com/users', params, function(data){
+    var myData = data.login;
     console.log(myData);
-    showResults(data.items);
+    // showResults(data.items);
      
-    function showResults(results){
-      $('.search-results').empty();
-      for (var i = 0; i < results.length; i++) {
-        var vidId = results[i].id.videoId;
-        console.log(vidId);
-        var addThumbnail = '<span><a href="https://www.youtube.com/watch?v='+ vidId +'"><img src="https://i.ytimg.com/vi/' + vidId+ '/default.jpg"></a></span><p>';
-        $('.search-results').append(addThumbnail);
-        console.log(vidId);
-        };
-      }
+    // function showResults(results){
+    //   $('.search-results').empty();
+    //   for (var i = 0; i < results.length; i++) {
+    //     var vidId = results[i].id.videoId;
+    //     console.log(vidId);
+    //     var addThumbnail = '<span><a href="https://www.youtube.com/watch?v='+ vidId +'"><img src="https://i.ytimg.com/vi/' + vidId+ '/default.jpg"></a></span><p>';
+    //     $('.search-results').append(addThumbnail);
+    //     console.log(vidId);
+    //     };
+    //   }
     });
   });  
 });
-
-oauth: c9c6541be312c9977298caed1b3057308ac1c638
