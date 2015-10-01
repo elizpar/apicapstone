403b27416a9fedc08ac7fa926c9471994ead57e8
Liz <elizpar@gmail.com>
2015-10-01 15:25:51 -0400

    lots of changes

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-$
+
 $(function(){
 
   $("#search-term").submit(function(event){
     event.preventDefault();
     var searchTerm = $('#query').val();
+    $('.search-results').empty();
 
     var params = {
-      part: '',
-      key: 'auth.js',
+      
+      key: window.authkey,
       q: searchTerm,
       r: 'json'
     };
 
-    $.getJSON('https://api.github.com/users', params, function(data){
-    var myData = data.login;
-    console.log(myData);
-    // showResults(data.items);
-     
+$.getJSON('https://api.github.com/search/users', params, function(data){
+  console.log(data.items[0].login);
+  for (var i = 0; i < data.items.length; i++) {
+    var login = data.items[i].login;
+    var url = data.items[i].avatar_url;
+    $('.search-results').append('<li>' + login + '<br><img src='+ url +'></li><br>');
+  }
+  
+
+   //'https://api.github.com/search/repositories?q=language:' +searchTerm+ 'sort=stars&order=desc'
     // function showResults(results){
-    //   $('.search-results').empty();
-    //   for (var i = 0; i < results.length; i++) {
-    //     var vidId = results[i].id.videoId;
-    //     console.log(vidId);
-    //     var addThumbnail = '<span><a href="https://www.youtube.com/watch?v='+ vidId +'"><img src="https://i.ytimg.com/vi/' + vidId+ '/default.jpg"></a></span><p>';
-    //     $('.search-results').append(addThumbnail);
-    //     console.log(vidId);
-    //     };
-    //   }
+      //multicursor
+    //     // var addThumbnail = '<span><a href="https://www.youtube.com/watch?v='+ vidId +'"><img src="https://i.ytimg.com/vi/' + vidId+ '/default.jpg"></a></span><p>';
+        
+   //     };
+      
     });
   });  
 });
