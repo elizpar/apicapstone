b17d4bcaa4c03f59c963369ad7ac7b46709946b5
Liz <elizpar@gmail.com>
2015-10-04 18:44:06 -0400

    working

--- app.js
+++ app.js
@@ -9,20 +9,21 @@ $(function(){
     var params = {
       
       key: window.authkey,
-      q: searchTerm,
-      r: 'json'
+      q: 'language:' + searchTerm
     };
 
-$.getJSON('https://api.github.com/search/users', params, function(data){
+$.getJSON('https://api.github.com/search/repositories', params, function(data){
+  console.log(data);
   console.log(data.items[0].login);
   for (var i = 0; i < data.items.length; i++) {
-    var login = data.items[i].login;
-    var url = data.items[i].avatar_url;
-    $('.search-results').append('<li>' + login + '<br><img src='+ url +'></li><br>');
+    var login = data.items[i].owner.login;
+    var avatar = data.items[i].owner.avatar_url;
+    var url = data.items[i].owner.html_url;
+    $('.search-results').append('<li>' + login + '<br><a href=' + url +'> <img src='+ avatar +'></a></li><br>');
   }
   
 
-   //'https://api.github.com/search/repositories?q=language:' +searchTerm+ 'sort=stars&order=desc'
+   
     // function showResults(results){
       //multicursor
     //     // var addThumbnail = '<span><a href="https://www.youtube.com/watch?v='+ vidId +'"><img src="https://i.ytimg.com/vi/' + vidId+ '/default.jpg"></a></span><p>';
